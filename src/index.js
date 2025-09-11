5ce9e97507fa5acaef6010be628bb7a2b440dd47
sabahatyns <sabahatyounas7@gmail.com>
2025-09-11 20:41:11 +0500

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// import mongoose from "mongoose";
+// import { DB_NAME} from "./constants.js";
+// import e from "express";
+import connectDB from "./db/db.js";
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+
+// there are two ways to do that. one is write all code in index.js like below,
+//  second is write in separate file in db folder and import here
+
+// ; (async () => {
+//     try {
+//         mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.log("error", error)
+//             throw error
+//         })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("error", error)
+//         throw error
+//     }
+// })() // IIFE in javascript
+
