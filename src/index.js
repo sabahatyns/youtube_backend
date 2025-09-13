0a444ff24175935abbca66f992cc574b1742bb49
sabahatyns <sabahatyounas7@gmail.com>
2025-09-13 12:49:38 +0500

    utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import mongoose from "mongoose";
 // import { DB_NAME} from "./constants.js";
 // import e from "express";
+import { app } from "./app.js";
 import connectDB from "./db/db.js";
 import dotenv from "dotenv"
 
@@ -8,6 +9,19 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is lisenting at port ${process.env.PORT}`)
+        })
+        app.on("error", (error) => {
+            console.log("error", error)
+            throw error
+        })
+    })
+    .catch((error) => {
+        console.log("db connection failed", error)
+    })
+
 
 // there are two ways to do that. one is write all code in index.js like below,
 //  second is write in separate file in db folder and import here
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+// there are two ways to do that one is using try catch and second is using promises
+// using promises
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((error) => next(error))
+    }
+}
+
+export { asyncHandler }
+
+
+
+//wrapper function using try catch
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
